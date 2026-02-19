eff6ea65f47926998719757f00f55cfe75d33452
YodkhwanA <yodkhwanpare@gmail.com>
2026-02-19 11:24:59 +0700

    Lobby

---
+++ Assets/Script/Core/Coin/Coin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Unity.Netcode;
+
+public abstract class Coin : NetworkBehaviour
+{ //เขียนแบบนี้เพื่อให้คลาสลูกสามารถใช้งานกับคลาสอื่นๆต่อได้
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    protected int coinValue = 10;
+
+    protected bool alreadyCollected;
+
+    public abstract int Collect();
+
+    public void SetValue(int value)
+    {
+        coinValue = value;
+    }
+
+    //main core coin
+    protected void Show(bool show)
+    {
+        spriteRenderer.enabled = show;
+    }
+}
+
+
---
+++ Assets/Script/Core/Coin/CoinWallet.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class CoinWallet : NetworkBehaviour
+{
+    public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
+
+    public void SpendCoins(int costToFire)
+    {
+        TotalCoins.Value -= costToFire;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.TryGetComponent<Coin>(out Coin coin)) { return; }
+
+        int coinValue = coin.Collect();
+
+        if (!IsServer) { return; }
+
+        TotalCoins.Value += coinValue;
+    }
+}
+
+
+//เขียนเพื่อให้ผู้เล่นเก็บเหรียญได้ถ้า coin มี colider โดยจะมีเงื่อนไขว่าต้องเป็นใน server ถึงจะเก็บเหรียญเข้ากระเป๋าได้
+//เรียนจะหายไปทั้ง server และ client แต่ค่าเหรียญจะเข้าที่ client
+//ยิงแล้วเหรียญลดด้วย
